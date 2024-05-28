00342f8504e7bbac2c18e6c3eecccb061592176f
devCarlosAlexandre <carloalexandresousa@gmail.com>
2024-05-28 09:35:20 -0300

    test: EmailValidatorAdapter returns false if validator return false

---
+++ src/utils/email-validator-adapter.spec.ts
@@ -0,0 +1,9 @@
+import { EmailValidatorAdapter } from "./email-validator";
+
+describe('EmailValidator Adpter', () => {
+    test('should return false if validator returns false', () => {
+        const sut = new EmailValidatorAdapter()
+        const isValid = sut.isValid('invalid_email@mail.com');
+        expect(isValid).toBe(false)
+    })
+})

4c2562b31cc2005c7b5312819688fb7fba4ba233
devCarlosAlexandre <carloalexandresousa@gmail.com>
2024-05-28 09:16:46 -0300

    chore: :test_tube: add config jest

--- jest.config.ts
+++ jest.config.ts
@@ -1,11 +1,8 @@
 import type { Config } from 'jest';
 
 const config: Config = {
-  clearMocks: true,
-  collectCoverage: true,
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
   coverageDirectory: "coverage",
-  coverageProvider: "v8",
   testEnvironment: "node",
   roots: [
     "<rootDir>/src"

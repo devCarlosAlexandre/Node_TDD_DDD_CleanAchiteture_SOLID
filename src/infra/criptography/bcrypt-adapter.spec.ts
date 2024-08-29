182d95f85bddff14557a0a6069e2f2129288d7cd
devCarlosAlexandre <carloalexandresousa@gmail.com>
2024-08-29 10:47:16 -0300

    refactor: create sut to a facotry help method

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -7,18 +7,22 @@ jest.mock('bcrypt', () => ({
     }
 }))
 
+const salt = 12;
+
+const makeSut = (): BcryptAdapter => {
+    return new BcryptAdapter(salt)
+}
+
 describe('Bcrypt Adpater', () => {
     test('Should call bcrypt with correct value', async () => {
-        const salt = 12
-        const sut = new BcryptAdapter(salt)
+        const sut = makeSut()
         const hashSpy = jest.spyOn(bcrypt, 'hash')
         await sut.encrypt('any_value')
         expect(hashSpy).toHaveBeenCalledWith('any_value', salt)
     })
 
     test('Should return a haseh on sucess', async () => {
-        const salt = 12
-        const sut = new BcryptAdapter(salt)
+        const sut = makeSut()
         const hash = await sut.encrypt('any_value')
         expect(hash).toBe("hash")
     })

9addd9e10a88a0f78d50c788047337a3d11f9828
devCarlosAlexandre <carloalexandresousa@gmail.com>
2024-09-03 11:08:14 -0300

    test: ensure cors is enable

--- src/main/config/middllewares.ts
+++ src/main/config/middllewares.ts
@@ -1,6 +1,8 @@
 import { Express } from "express"
 import { bodyParser } from "../middllewares/body-parser"
+import { cors } from "../middllewares/cors";
 
 export default (app: Express): void => {
     app.use(bodyParser);
+    app.use(cors);
 }

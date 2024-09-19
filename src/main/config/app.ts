8699b369d31b4fe74e14c721a983d74e88958706
devCarlosAlexandre <carloalexandresousa@gmail.com>
2024-09-19 12:02:27 -0300

    feature: ensure signup route return an account on success

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import setupMiddllewares from './middllewares';
+import setupRoutes from './routes';
 
 const app = express();
 setupMiddllewares(app);
+setupRoutes(app);
 export default app;

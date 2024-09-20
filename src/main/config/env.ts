576cd639a98a6f157a49c32f240aacf1e43aa639
devCarlosAlexandre <carloalexandresousa@gmail.com>
2024-09-20 14:33:43 -0300

    feature: connect to mongodb before app listen

---
+++ src/main/config/env.ts
@@ -0,0 +1,4 @@
+export default {
+    mongoUrl: 'mongodb://127.0.0.1:27017/clean-node-api',
+    port: process.env.PORT || 5050
+}
--- src/main/server.ts
+++ src/main/server.ts
@@ -1,3 +1,10 @@
-import app from "./config/app";
 
-app.listen(3000, () => console.log("Server running at http://localhost:3000"));
+import { MongoHelper } from '../infra/db/mongodb/helpers/mongo-helper';
+import env from './config/env';
+
+MongoHelper.connect(env.mongoUrl)
+    .then(async () => {
+        const app = (await import("./config/app")).default
+        app.listen(env.port, () => console.log(`Server running at http://127.0.0.1:${env.port}`));
+    })
+    .catch(console.error);

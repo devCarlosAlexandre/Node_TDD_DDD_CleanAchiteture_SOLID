258334cfa6f27f3ca6a2dfb5b94a99893501879e
devCarlosAlexandre <carloalexandresousa@gmail.com>
2024-09-24 13:52:41 -0300

    alter script test

--- jest.config.ts
+++ jest.config.ts
@@ -1,7 +1,10 @@
 import type { Config } from 'jest';
 
 const config: Config = {
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/main/**',
+  ],
   coverageDirectory: "coverage",
   testEnvironment: "node",
   preset: '@shelf/jest-mongodb',

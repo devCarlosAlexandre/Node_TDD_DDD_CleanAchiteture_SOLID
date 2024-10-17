ed0b876dcfdd3e7ce8079f829666ce13d62efa1b
devCarlosAlexandre <carloalexandresousa@gmail.com>
2024-10-17 12:06:45 -0300

    feat: ensure LogControllerDecoretor calls controller with correct value

--- src/presentation/errors/invalid-param-error.ts
+++ src/presentation/errors/invalid-param-error.ts
@@ -1,6 +1,6 @@
 export class InvalidParamError extends Error {
     constructor(paramName: string) {
-        super(`Invalid param: ${paramName}`)
+        super(`Invalid params: ${paramName}`)
         this.name = 'InvalidParamError'
     }
 }
